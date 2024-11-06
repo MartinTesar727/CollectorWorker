f647df2048f15f0dcf05c3866f45de116e703dd8
MartinTesar727 <mtesar467@seznam.cz>
2024-11-06 09:34:11 +0100

    Clean slate

--- UsageCollectorWorkerService/Models/UsageValues.cs
+++
@@ -1,8 +0,0 @@
-namespace UsageCollectorWorkerService.Models;
-
-public class UsageValues
-{
-    public long UtcTimeStamp { get; set; }
-    public byte CpuUsageInPercentage { get; set; }
-    public byte RamUsageInPercentage { get; set; }
-}
--- UsageCollectorWorkerService/Program.cs
+++ UsageCollectorWorkerService/Program.cs
@@ -7,21 +7,7 @@ namespace UsageCollectorWorkerService
     {
         public static void Main(string[] args)
         {
-            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
             
-            builder.Services.AddSingleton<HttpClient>(_ => new HttpClient()
-            {
-                /*BaseAddress = new Uri(args[0]),*/
-                BaseAddress = new Uri($"http://10.1.103.190:5000/api/resourceusage/save"),
-                Timeout = TimeSpan.FromSeconds(10)
-            });
-            builder.Services.AddSingleton<ICustomHttpClient, CustomHttpClient>();
-            builder.Services.AddSingleton<IGetSystemResources, GetSystemResourcesLinux>();
-            
-            builder.Services.AddHostedService<Worker>();
-
-            IHost host = builder.Build();
-            host.Run();
         }
     }
 }
